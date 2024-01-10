5736a1ab112f40b5d323a1ed0bef38fe8c3b6b9e
Hellicat <jhccop@naver.com>
2024-01-10 16:05:56 +0900

    PORT env variable

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import "./models/commentModel";
 
 import app from "./server";
 
-const PORT = 5040;
+const PORT = process.env.PORT || 5040;
 
 const handleListen = () =>
 	console.log("✅ Server is Listenin on http://localhost:5040");
